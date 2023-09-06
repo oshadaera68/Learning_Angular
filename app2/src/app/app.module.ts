ece2c2c479dcd34d9ae81985c394d360ef5d0b04
oshadaera68 <mindartlk010@gmail.com>
2023-09-06 13:38:23 +0530

    'updated'

--- app2/src/app/app.module.ts
+++ app2/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HomeComponent} from "./home/home.component";
+import { FormsModule } from '@angular/forms';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app2/src/app/home/home.component.ts
+++ app2/src/app/home/home.component.ts
@@ -1,11 +1,23 @@
-import {Component} from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  name: string = 'Samantha B'
-  age: number = 52
+ 
+  /* One Way Data Binding */
+  // setData(value:string){
+  //  console.log(value);
+   
+  // }
+  // name: string = 'Samantha B'
+  // age: number = 52
+
+  // image='https://www.treehugger.com/thmb/IX5hq-NRXj9YTfKB4qK6qbdT9Hg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/11-facts-about-blue-whales-largest-animals-ever-known-earth-ADD-FINAL-a99cc86cbc0c49c3bd3e9265203ef7f4.png'
+  // width=400;
+
+    /* Two Way Data Binding */
+    name:string = '';
 }
