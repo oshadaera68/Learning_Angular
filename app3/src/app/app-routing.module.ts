b8991697e1598ac8623651d5b219b5e8aa79c040
oshadaera68 <mindartlk010@gmail.com>
2023-09-06 22:00:10 +0530

    add project and added modules

--- app3/src/app/app-routing.module.ts
+++ app3/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'starter', pathMatch:'full'},
+  {path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule)},
+  {path: 'starter', loadChildren: () => import('./modules/starter/starter.module').then(m => m.StarterModule)},
+  {path:'**', component:NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
