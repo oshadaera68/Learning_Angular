a2d4b35e9e7820b1cf06c3131c376be3d95e4428
oshadaera68 <mindartlk010@gmail.com>
2023-09-18 16:51:40 +0530

    added directives

--- http-app/src/app/app.module.ts
+++ http-app/src/app/app.module.ts
@@ -1,30 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AllComponent } from './components/all/all.component';
-import { NewComponent } from './components/new/new.component';
-import { UpdateComponent } from './components/update/update.component';
-import { DeleteComponent } from './components/delete/delete.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FindComponent } from './components/find/find.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AllComponent} from './components/all/all.component';
+import {NewComponent} from './components/new/new.component';
+import {UpdateComponent} from './components/update/update.component';
+import {DeleteComponent} from './components/delete/delete.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FindComponent} from './components/find/find.component';
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AllComponent,
-    NewComponent,
-    UpdateComponent,
-    DeleteComponent,
-    FindComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AllComponent,
+        NewComponent,
+        UpdateComponent,
+        DeleteComponent,
+        FindComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- http-app/src/app/components/all/all.component.ts
+++ http-app/src/app/components/all/all.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-all',
   templateUrl: './all.component.html',
   styleUrls: ['./all.component.scss']
 })
-export class AllComponent {
+export class AllComponent implements OnInit{
+
+  list:Array<any>=[];
+
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>("https://jsonplaceholder.typicode.com/posts").subscribe(respone=>{
+      console.log(respone)
+      this.list=respone;
+      console.log(this.list)
+    });
+  }
 
 }
