ab89876f41e0985f8708ed7589f7b10d3110f547
oshadaera68 <mindartlk010@gmail.com>
2023-09-25 15:07:52 +0530

    added env files

---
+++ app1/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAT6V5Z9-MT6mXUJw_wsQ3R4RHBhQjN13U",
+    authDomain: "fir-auth-cf206.firebaseapp.com",
+    projectId: "fir-auth-cf206",
+    storageBucket: "fir-auth-cf206.appspot.com",
+    messagingSenderId: "827602242135",
+    appId: "1:827602242135:web:3dd3c116c91cbf18773112",
+    measurementId: "G-CW4FMRXY0S"
+  }
+};
